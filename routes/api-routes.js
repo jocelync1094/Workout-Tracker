424be89bc3abfc887eac8689c7f76595b4a2613a
Jocelyn Chang <jocelync1094@gmail.com>
2020-02-08 20:56:51 -0800

    add api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,14 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+router.get("/exercise", (req,res) => {
+    res.redirect("exercise.html");
+})
+
+router.get("/stats", (req,res) => {
+    res.redirect("stats.html");
+})
+
 router.get("/api/workouts", (req, res) => {
     Workout.find({}).sort({day:1}).then(dbWorkout => {
         res.json(dbWorkout);
@@ -12,6 +20,31 @@ router.get("/api/workouts", (req, res) => {
 router.get("/api/workouts/range", (req, res) => {
     Workout.find({}).sort({exercise:-1}).then(dbWorkout => {
         res.json(dbWorkout);
+    }).catch(err => {
+        res.status(400).json(err);
+    })
+})
+
+router.get("/api/exercise/:id", (req,res) => {
+    Workout.find({_id: mongojs.ObjectId(req.params.id)}).then(dbExercise => {
+        res.json(dbExercise);
+    }).catch(err => {
+        res.status(400).json(err);
+    })
+})
+
+router.post("api/workouts", ({body},res) => {
+    Workout.create({body}).then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.status(400).json(err);
+  
+    })
+})
+
+router.post("/api/workouts/:id", ({body},res) => {
+    Workout.findByIdAndUpdate({_id: mongojs.ObjectId(req.params.id)},{ $push: {exercise: body}}).then(dbExercise => {
+        res.json(dbExercise);
     }).catch(err => {
         res.status(400).json(err);
     })
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
-const PORT = process.env.PORT || 8080
+const PORT = 8080
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
